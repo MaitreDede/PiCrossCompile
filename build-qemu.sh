567657d74743a9fccb108729f27a56463b9e1344
Damien <ddaly@isi.nc>
2017-10-04 10:41:22 +1100

    if case

--- build-qemu.sh
+++ build-qemu.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
-set -o errexit
-set -o nounset
-set -o pipefail
 
-case ${QEMU_VERSION} in
-"COMPILED")
+if [ "${QEMU_VERSION}" = "COMPILED" ]
+then
     echo "Compiling latest qemu"
     source build-qemu-compile.sh
-    ;;
-"SYSTEM")
-    echo "Using system qemu"
-    source build-qemu-system.sh
-    ;;
-*)
-    echo "WARNING : unspecified or invalid value for QEMU_VERSION. Valid values are COMPILED,SYSTEM. Found '${QEMU_VERSION}'"
-    echo "Assuming default: SYSTEM"
-    source build-qemu-system.sh
-    ;;
-esac
+else
+    if [ "${QEMU_VERSION}" = "SYSTEM" ]
+        echo "Using system qemu"
+        source build-qemu-system.sh
+    else
+        echo "WARNING : unspecified or invalid value for QEMU_VERSION. Valid values are COMPILED,SYSTEM. Found '${QEMU_VERSION}'"
+        echo "Assuming default: SYSTEM"
+        source build-qemu-system.sh
+    fi
+fi
